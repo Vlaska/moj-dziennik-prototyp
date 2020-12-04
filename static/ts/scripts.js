8bd74e7734325c689efc167e79a73d793836322e
Vlaska <vlaska8888@gmail.com>
2020-12-04 20:42:33 +0100

    Base for adding new grades

---
+++ static/ts/scripts.js
@@ -0,0 +1,15 @@
+function init() {
+    var grade_container = $('#grade-container');
+    for (var _i = 0, _a = grade_container.children(); _i < _a.length; _i++) {
+        var i = _a[_i];
+        console.log(i);
+    }
+}
+/**
+ *
+ * @param {HTMLElement} src
+ */
+function set_grade(src) {
+    var selection = $('#selected-grade');
+    console.log(selection);
+}
--- static/ts/scripts.ts
+++ static/ts/scripts.ts
@@ -1,14 +1,16 @@
-"use strict";
+
+
 function init_grades() {
     let grade_container = $('#grades-buttons');
     let to_init = grade_container.children();
     for (let i = 0; i < to_init.length; ++i) {
+
         $(to_init[i]).on("click", () => {
             set_grade($(to_init[i]));
         });
     }
     $('#selected-grade').on('click', reset_selected_grade);
-    let rows = $('#grade-table').get()[0].rows;
+    let rows = ($('#grade-table').get()[0] as HTMLTableElement).rows;
     for (let i = 1; i < rows.length; ++i) {
         let cells = rows[i].cells;
         for (let j = 1; j < cells.length; ++j) {
@@ -20,15 +22,20 @@ function init_grades() {
         }
     }
 }
+
+
 function reset_selected_grade() {
     let selected_container = $('#selected-grade');
+
     if (selected_container.html() === "<i class=\"fas fa-mouse-pointer\"></i>") {
         return;
     }
+
     if (selected_container.data('trash')) {
         selected_container.removeClass('trash');
         selected_container.data('trash', false);
     }
+
     selected_container.html("<i class=\"fas fa-mouse-pointer\"></i>");
     let t = $('.grade-select-menu-item.selected');
     if (t) {
@@ -36,35 +43,38 @@ function reset_selected_grade() {
     }
     $('#empty-grade').addClass('selected');
 }
-function set_grade(src) {
+
+function set_grade(src: JQuery<HTMLElement>) {
     let selection = $('#selected-grade');
+
     if (selection.data('trash') == true) {
-        console.log(src.data('trash'));
+        console.log(src.data('trash'))
         if (src.data('trash') == false || src.data('trash') == undefined) {
             selection.removeClass('trash');
-            selection.data('trash', false);
-            selection.data('selected', src.data('selected'));
+            selection.data('trash', false)
+            selection.data('selected', src.data('selected'))
             selection.text(src.data('selected'));
+
             let t = $('.grade-select-menu-item.selected');
             if (t) {
                 t.removeClass('selected');
             }
             src.addClass('selected');
         }
-    }
-    else {
+    } else {
         if (src.data('trash') == true) {
             selection.addClass('trash');
-            selection.data('trash', true);
-            selection.data('selected', '');
+            selection.data('trash', true)
+            selection.data('selected', '')
+
             selection.html('<i class="far fa-trash-alt">');
+
             let t = $('.grade-select-menu-item.selected');
             if (t) {
                 t.removeClass('selected');
             }
-        }
-        else {
-            selection.data('selected', src.data('selected'));
+        } else {
+            selection.data('selected', src.data('selected'))
             selection.html(src.data('selected'));
             let t = $('.grade-select-menu-item.selected');
             if (t) {
@@ -75,19 +85,19 @@ function set_grade(src) {
     }
     if (src.data('pointer')) {
         selection.data('pointer', true);
-    }
-    else {
+    } else {
         selection.data('pointer', false);
     }
 }
-function operation_on_cell(src) {
+
+function operation_on_cell(src: JQuery<HTMLTableCellElement>) {
     let grade = $('#selected-grade');
+    
     if (grade.data('pointer')) {
-    }
-    else if (grade.data('trash')) {
+
+    } else if (grade.data('trash')) {
         src.text('');
-    }
-    else {
+    } else {
         src.text(grade.data('selected'));
     }
     // console.log(grade.data('selected'));
